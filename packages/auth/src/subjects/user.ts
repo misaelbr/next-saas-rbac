e4a1c59ba6784e0b82b68db6a9a71ba1206ac298
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2024-04-24 11:11:35 -0300

    feat: config permissions

---
+++ packages/auth/src/subjects/user.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod'
+
+export const userSubject = z.tuple([
+  z.union([
+    z.literal('manage'),
+    z.literal('get'),
+    z.literal('delete'),
+    z.literal('invite'),
+    z.literal('update'),
+  ]),
+  z.literal('User'),
+])
+
+export type UserSubject = z.infer<typeof userSubject>

d9279b85b5d5d6593481834ed4fe38aa82ce48db
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2024-04-26 16:50:46 -0300

    feat(auth): add recover password with send token

---
+++ apps/api/src/http/routes/auth/reset-password.ts
@@ -0,0 +1,54 @@
+import { hash } from 'bcryptjs'
+import type { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+import { UnauthorizedError } from '../_errors/unauthorized-error'
+
+export async function resetPassword(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/password/reset',
+    {
+      schema: {
+        tags: ['auth'],
+        summary: 'Reset password',
+        description: 'Receive new password after passaword token validation',
+        body: z.object({
+          code: z.string(),
+          password: z.string().min(6),
+        }),
+        response: {
+          201: z.null(),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { code, password } = request.body
+
+      const tokenFromCode = await prisma.token.findUnique({
+        where: { id: code },
+      })
+
+      if (!tokenFromCode) {
+        throw new UnauthorizedError('Invalid or expired token')
+      }
+
+      const passwordHash = await hash(password, 6)
+
+      await prisma.user.update({
+        where: { id: tokenFromCode.userId },
+        data: {
+          passwordHash,
+        },
+      })
+
+      await prisma.token.delete({
+        where: { id: code },
+      })
+
+      return reply.status(201).send()
+    },
+  )
+}

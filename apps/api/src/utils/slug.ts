732e13f93f0832ee1192ba0acc5d93f427e072ef
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2024-04-29 17:16:55 -0300

    feat: add support to create organizations

---
+++ apps/api/src/utils/slug.ts
@@ -0,0 +1,9 @@
+export function createSlug(text: string): string {
+  return text
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/[^a-zA-Z0-9]/g, '-')
+    .replace(/-+/g, '-')
+    .replace(/^-|-$/g, '')
+    .toLowerCase()
+}

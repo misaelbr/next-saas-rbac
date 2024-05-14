04bb5bc4457e38f1bef5942595de8df8673a6141
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2024-05-13 21:05:18 -0300

    feat: add update organization

---
+++ apps/api/src/utils/get-user-permissions.ts
@@ -0,0 +1,12 @@
+import { defineAbilitiyFor, type Role, userSchema } from '@saas/auth'
+
+export function getUserPermissions(userId: string, role: Role) {
+  const authUser = userSchema.parse({
+    id: userId,
+    role,
+  })
+
+  const ability = defineAbilitiyFor(authUser)
+
+  return ability
+}

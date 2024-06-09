439652ea80c996376fb4c5fa2f30b5fbb84046b9
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2024-06-09 12:23:55 -0300

    feat: add supporto to get org members

---
+++ apps/api/src/http/routes/members/get-members.ts
@@ -0,0 +1,95 @@
+import type { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { getUserPermissions } from '@/utils/get-user-permissions'
+
+import { UnauthorizedError } from '../_errors/unauthorized-error'
+
+export async function getMembers(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations/:slug/members',
+      {
+        schema: {
+          tags: ['Members'],
+          summary: 'Get all organization members',
+          description: 'Get all organization members by its slug',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            200: z.object({
+              membersWithRole: z.array(
+                z.object({
+                  userId: z.string(),
+                  id: z.string(),
+                  role: z.union([
+                    z.literal('ADMIN'),
+                    z.literal('MEMBER'),
+                    z.literal('BILLING'),
+                  ]),
+                  name: z.string().nullable(),
+                  avatarUrl: z.string().url().nullable(),
+                  email: z.string().email(),
+                }),
+              ),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug } = request.params
+
+        const userId = await request.getCurrentUserId()
+        const { organization, membership } =
+          await request.getUserMembership(slug)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('get', 'User')) {
+          throw new UnauthorizedError(
+            `You're not allowed to see organization members`,
+          )
+        }
+
+        const members = await prisma.member.findMany({
+          select: {
+            id: true,
+            role: true,
+            user: {
+              select: {
+                id: true,
+                name: true,
+                email: true,
+                avatarUrl: true,
+              },
+            },
+          },
+          where: {
+            organizationId: organization.id,
+          },
+          orderBy: {
+            role: 'asc',
+          },
+        })
+
+        const membersWithRole = members.map(
+          ({ user: { id: userId, ...user }, ...member }) => {
+            return {
+              ...user,
+              ...member,
+              userId,
+            }
+          },
+        )
+
+        return reply.send({ membersWithRole })
+      },
+    )
+}
